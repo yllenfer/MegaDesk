df3ab5dbe42273b6e45ecf366a7f7065abe85c4c
Ian <Ian@GEOFF-LENOVO>
2023-05-16 12:11:55 -0600

    add text file and method rushOrder

--- DeskQuote.cs
+++ DeskQuote.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.Design.Serialization;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Runtime.InteropServices;
@@ -8,6 +10,7 @@ using System.Runtime.Remoting.Messaging;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using System.Xml.Schema;
 using static MegaDesk.Desk;
 
@@ -24,12 +27,10 @@ namespace MegaDesk
         public DateTime date; 
         public Desk Desk { get; set; }
 
+        // Create a two dimensional array for RushOrder Method (Julieta White)
+        public static int[,] rushOrderPrices = new int[3, 3];
 
-
-
-
-
-        public DeskQuote(string date, string rushOrder, Desk desk)
+        public DeskQuote(string date, string rushOrder, int drawers, SurfaceMaterial surfaceMaterial, Desk desk)
         {
             Date = date.ToString();
             RushOrder = rushOrder;
@@ -111,7 +112,62 @@ namespace MegaDesk
         }
 
 
+        // Method GetRushOrder. This method will handle the population of a member
+        // variable that hold the dimensional array. (Julieta White)
+
+        /* two dimensional array
+         *      $60     $70     $80
+         *      $40     $50     $60
+         *      $30     $35     $40
+         */
 
 
+
+        public static int[,] GetRushOrder()
+        {
+            int[,] rushOrderPrices = new int[3, 3];
+            string[] priceList = new string[9];
+            priceList = ReadRushOrder();
+            int count = 0;
+            
+            while (count < priceList.Length)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j< 3; j++)
+                    {
+                        rushOrderPrices[i, j] = int.Parse(priceList[count]);
+                        count ++;
+
+                    }
+                }
+            }
+            return rushOrderPrices;
+        }
+        
+        public static string[] ReadRushOrder()
+        {
+            string[] priceList = new string[9];
+            try
+            {
+                string filePath = @"C:\Users\Ian\Desktop\MegaDesk_Group\Resources\rushOrderPrices.txt";
+                StreamReader reader = new StreamReader(filePath);
+                int i = 0;
+                while (reader.EndOfStream == false)
+                {
+                    string line = reader.ReadLine();
+                    priceList[i] = line;
+                    i++;
+                }
+                reader.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Something went wrong." + e.Message);
+                Application.Exit();
+            }
+            return priceList;
+        }
     }
+
 }
--- SearchQuotes.Designer.cs
+++ SearchQuotes.Designer.cs
@@ -36,19 +36,43 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(231, 212);
+            this.label1.Location = new System.Drawing.Point(616, 191);
+            this.label1.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(144, 25);
+            this.label1.Size = new System.Drawing.Size(359, 58);
             this.label1.TabIndex = 0;
             this.label1.Text = "Search Quotes";
             // 
+            // searchBox
+            // 
+            this.searchBox.FormattingEnabled = true;
+            this.searchBox.Location = new System.Drawing.Point(548, 362);
+            this.searchBox.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(516, 39);
+            this.searchBox.TabIndex = 1;
+            this.searchBox.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // deskQuotesListBox
+            // 
+            this.deskQuotesListBox.FormattingEnabled = true;
+            this.deskQuotesListBox.ItemHeight = 31;
+            this.deskQuotesListBox.Location = new System.Drawing.Point(307, 489);
+            this.deskQuotesListBox.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.deskQuotesListBox.Name = "deskQuotesListBox";
+            this.deskQuotesListBox.Size = new System.Drawing.Size(967, 469);
+            this.deskQuotesListBox.TabIndex = 2;
+            // 
             // SearchQuotes
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(580, 454);
+            this.ClientSize = new System.Drawing.Size(1547, 1083);
+            this.Controls.Add(this.deskQuotesListBox);
+            this.Controls.Add(this.searchBox);
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.Name = "SearchQuotes";
             this.Text = "Search Quotes";
             this.ResumeLayout(false);
